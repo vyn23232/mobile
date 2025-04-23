17d6b8bd18019f7a727cb6ca63822f2087214912
vyn <jhovynn3@gmail.com>
2025-04-23 23:28:50 +0800

    added 13 files

---
+++ CapstoneConnect/app/src/main/java/com/mobile/capstoneconnect/SetupActivity2.kt
@@ -0,0 +1,13 @@
+package com.mobile.capstoneconnect
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SetupActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ CapstoneConnect/app/src/main/java/com/mobile/capstoneconnect/SignupActivity.kt
@@ -0,0 +1,11 @@
+package com.mobile.capstoneconnect
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
